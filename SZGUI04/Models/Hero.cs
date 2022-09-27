2c1792fabc6c7b5fb66c6b417b1cf8ec0812118c
hallgato <hallgato@LABOR215-23>
2022-09-27 13:03:14 +0200

    model

---
+++ SZGUI04/Models/Hero.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGUI04.Models
+{
+    public enum Side { jo, gonosz, semleges }
+    public class Hero : ObservableObject
+    {
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private int power;
+        public int Power
+        {
+            get { return power; }
+            set { SetProperty(ref power, value); }
+        }
+
+        private int speed;
+        public int Speed
+        {
+            get { return speed; }
+            set { SetProperty(ref speed, value); }
+        }
+        private Side oldal;
+
+        public Side Oldal
+        {
+            get { return oldal; }
+            set { oldal = value; }
+        }
+
+    }
+}
